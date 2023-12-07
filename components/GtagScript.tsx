0b0abb063880637105c642691b2694f4961d799c
Andreas Søgaard Pedersen <andreassoegaard@Andreass-MBP.jbdeveloper.jyskebank.dk>
2023-12-07 10:17:55 +0100

    updates

--- components/GtagScript.tsx
+++ components/GtagScript.tsx
@@ -1,4 +1,5 @@
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 export const GtagScript: React.FunctionComponent = () => {
     return (
@@ -11,7 +12,6 @@ export const GtagScript: React.FunctionComponent = () => {
                 gtag('js', new Date());
         
                 gtag('config', 'AW-11185102926');
-                gtag('event', 'conversion', {'send_to': 'AW-11185102926/_kmiCIK8p_0YEM7AvNUp'})
                 `}
             </Script>
         </>
