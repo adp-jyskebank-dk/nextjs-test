683b858caa5280939130475d7c89f612783e333e
Andreas Søgaard Pedersen <andreassoegaard@Andreass-MBP.jbdeveloper.jyskebank.dk>
2023-12-07 09:25:35 +0100

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
+  const initGtag = () => {
+    if (typeof window !== 'undefined' && (window as any).dataLayer) {
+      (window as any).dataLayer.push({
+        config: 'AW-11185102926',
+        js: new Date(),
+      })
+    }
+  }
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script async src='https://www.googletagmanager.com/gtag/js?id=AW-11185102926' onLoad={initGtag} />
+      </Head>
       <body>
         <Main />
         <NextScript />
