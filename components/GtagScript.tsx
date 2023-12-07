818cf5e0bef7e30d6d77734e16cd642f087206b7
Andreas Søgaard Pedersen <andreassoegaard@Andreass-MBP.jbdeveloper.jyskebank.dk>
2023-12-07 10:42:00 +0100

    updates

--- components/GtagScript.tsx
+++ components/GtagScript.tsx
@@ -12,9 +12,6 @@ export const GtagScript: React.FunctionComponent = () => {
                 gtag('js', new Date());
         
                 gtag('config', 'AW-11185102926');
-                gtag('event', 'conversion', {
-                    'send_to': 'AW-11185102926/_kmiCIK8p_0YEM7AvNUp',
-                  });
                 `}
             </Script>
         </>
