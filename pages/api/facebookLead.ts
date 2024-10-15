eda0acff34ff3ca57efcf73952017186faaa02aa
Andreas Søgaard Pedersen <adp@jyskebank.dk>
2024-10-15 09:59:45 +0200

    updates#

--- pages/api/facebookLead.ts
+++ pages/api/facebookLead.ts
@@ -1,19 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const VERIFY_TOKEN = 'Clr1eJxsX5PbRmaTkHM6bIPeSJDJItjBaSc0TJ6lrlxJw'
+const FACEBOOK_PAGE_ACCESS_TOKEN = 'token'
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
+    if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+        return;
+    }
     console.log(
       'WEBHOOK GET request',
       `hub.verify_token: ${req.query['hub.verify_token']}`
     );
     res.status(200).send('GET request');
   } else if (req.method === 'POST') {
-    const body = JSON.stringify(req.body);
-    console.log('WEBHOOK POST request', `body: ${body}`);
-    res.status(200).send('POST request');
+    // Facebook will be sending an object called "entry" for "leadgen" webhook event
+    if (!req.body.entry) {
+        return res.status(500).send({ error: 'Invalid POST data received' });
+    }
+
+    // Travere entries & changes and process lead IDs
+    for (const entry of req.body.entry) {
+        for (const change of entry.changes) {
+            // Process new lead (leadgen_id)
+            await processNewLead(change.value.leadgen_id);
+        }
+    }
+
+    // Success
+    res.send({ success: true });
   } else if (req.method === 'OPTIONS') {
     console.log('WEBHOOK OPTIONS request');
     res.status(204).end();
@@ -21,4 +40,56 @@ export default async function handler(
     console.log('WEBHOOK BAD request');
     res.status(400).end();
   }
+}
+
+// Process incoming leads
+async function processNewLead(leadId: string) {
+    let response: any;
+
+    try {
+        // Get lead details by lead ID from Facebook API
+        response = await fetch(`https://graph.facebook.com/v9.0/${leadId}/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`);
+    }
+    catch (err: any) {
+        // Log errors
+        return console.warn(`An invalid response was received from the Facebook API:`, err.response.data ? JSON.stringify(err.response.data) : err.response);
+    }
+
+    // Ensure valid API response returned
+    if (!response.data || (response.data && (response.data.error || !response.data.field_data))) {
+        return console.warn(`An invalid response was received from the Facebook API: ${response}`);
+    }
+
+    // Lead fields
+    const leadForm = [];
+
+    // Extract fields
+    for (const field of response.data.field_data) {
+        // Get field name & value
+        const fieldName = field.name;
+        const fieldValue = field.values[0];
+
+        // Store in lead array
+        leadForm.push(`${fieldName}: ${fieldValue}`);
+    }
+
+    // Implode into string with newlines in between fields
+    const leadInfo = leadForm.join('\n');
+
+    // Log to console
+    console.log('A new lead was received!\n', leadInfo);
+
+    // Use a library like "nodemailer" to notify you about the new lead
+    // 
+    // Send plaintext e-mail with nodemailer
+    // transporter.sendMail({
+    //     from: `Admin <admin@example.com>`,
+    //     to: `You <you@example.com>`,
+    //     subject: 'New Lead: ' + name,
+    //     text: new Buffer(leadInfo),
+    //     headers: { 'X-Entity-Ref-ID': 1 }
+    // }, function (err) {
+    //     if (err) return console.log(err);
+    //     console.log('Message sent successfully.');
+    // });
 }
